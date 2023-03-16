3400562a6a5c45a0c01c096d507429a079fb96cf
KacperP0 <127994739+KacperP0@users.noreply.github.com>
2023-03-16 08:47:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+int prime(int num, int div);
 
-int main(void) {
-  printf("Hello world!");
-  return 0;
-}  
+int main() {
+    int number;
+    scanf("%d", &number);
+    if (prime(number, number-1)==1)
+        printf("Liczba pierwsza!");
+    else
+        printf("Liczba nie jest pierwsza!");
+    return 0;
+}
+
+int prime(int num, int div) {
+    if (num==0 || num==1)
+        return 0;
+    else if (div==1)
+        return 1;
+    else if ((num%div)==0)
+        return 0;
+    else
+        return prime(num, div-1);
+}
